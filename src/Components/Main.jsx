ed5adaad36ce10da0cbdfa6d587a4dadd2f5cafc
lucianoideh <vazquezluciano5@gmail.com>
2024-04-09 13:41:40 -0300

    removed comments & old function

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,29 +5,16 @@ import CardColection from "./CardColection"
 
 const Main = () => {
     const [isGame,setIsGame] = useState(false)
-    const [pokemon, setPokemon] = useState([])
-    const poolSize = 8 
-    // const url = "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"
-   
+    const [pokemon, setPokemon] = useState([])   
 
     function handleClick(){
         setIsGame(true)
-        getPokemon()
     }
 
     async function getRandomNum(){
         const max = 700
         return Math.floor(Math.random()*max)
     }
-
-//    async function getPokemon(){
-//     const randomNum = await getRandomNum()
-//      const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-//      const data = await result.json()
-//      setPokemon(data)
-//      console.log(data)
-
-//    }
 
    useEffect(() => {
     const getPokemons = async () => {
