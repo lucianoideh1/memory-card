798e74e5bbbb2007f3e92dca7ecde440f3e20d63
lucianoideh <vazquezluciano5@gmail.com>
2024-04-10 14:46:02 -0300

    implemented score current and best on clicks

--- src/Components/Scoreboard.jsx
+++ src/Components/Scoreboard.jsx
@@ -1,7 +1,25 @@
+import { useState,useEffect} from 'react'
 const Scoreboard = () => {
+
+  const [score, setScore] = useState({current:0,best:8})
+
+  function handleClick ()  {
+    setScore({...score,current:score.current + 1})
+  }
+ 
+  useEffect(() => {
+
+    function isBest(){
+      if(score.current > score.best){
+        setScore({...score,best : score.current})
+      }
+    }
+  
+  isBest()
+  },[score])
   return (
-    <div className="scoreboard">
-        Current Score: 0 | Best Score: 8
+    <div className="scoreboard" onClick={handleClick}>
+        Current Score: {score.current} | Best Score: {score.best}
     </div>
   )
 }
