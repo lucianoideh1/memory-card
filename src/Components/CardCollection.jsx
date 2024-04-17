abcbb6a9f3b4cfd2fc45e68d3406682e79936d6c
lucianoideh <vazquezluciano5@gmail.com>
2024-04-17 16:52:08 -0300

    promise all an array

--- src/Components/CardCollection.jsx
+++ src/Components/CardCollection.jsx
@@ -5,20 +5,43 @@ import Card from "./Card"
   
     const [loading, setLoading] = useState(true)
     
-    useEffect(() => {
-        // const getPokemons = async () => {
-        //   console.log("fetching pokemons")
-        //   const randomNum =  await getRandomNum()
-        //   const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-        //   const data = await  result.json()
-        //   setPokemons(data)
-        //   setLoading(false)
-        //   console.log(data)
-        //   console.log("game start, pokemons fetched")
-        // }
-        // getPokemons()
-        
-      },[setPokemons])
+    const getPokemonPool = (num) => {
+      console.log(`returns ${num} pokemons`)
+    }
+
+    useEffect(()=>{
+      const fetchArr = [
+        fetch("https://pokeapi.co/api/v2/pokemon/1"),
+        fetch("https://pokeapi.co/api/v2/pokemon/2"),
+        fetch("https://pokeapi.co/api/v2/pokemon/3"),
+        fetch("https://pokeapi.co/api/v2/pokemon/4"),
+        fetch("https://pokeapi.co/api/v2/pokemon/5"),
+        fetch("https://pokeapi.co/api/v2/pokemon/6"),
+        fetch("https://pokeapi.co/api/v2/pokemon/7"),
+        fetch("https://pokeapi.co/api/v2/pokemon/8")
+      ]
+  
+      Promise.all(fetchArr)
+      .then(data => data.json())
+      .then(data => console.log(data))
+      
+    },[])
+
+    // useEffect(() => {
+    //     const getPokemons = async () => {
+    //       console.log("fetching pokemons")
+    //       const randomNum =  await getRandomNum()
+    //       const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+    //       const data = await  result.json()
+    //       setPokemons(data)
+    //       setLoading(false)
+    //       console.log(data)
+    //       console.log("game start, pokemons fetched")
+    //     }
+    //     getPokemons()
+    //     getPokemonPool(8)
+
+    //   },[setPokemons])
 
        return(
         <div>
