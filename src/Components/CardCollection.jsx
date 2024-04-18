5e2b1fd604532e1c423e4b21b0fed3bee6072c02
lucianoideh <vazquezluciano5@gmail.com>
2024-04-18 15:46:55 -0300

    getting the data for cards

--- src/Components/CardCollection.jsx
+++ src/Components/CardCollection.jsx
@@ -1,51 +1,72 @@
 import {useState, useEffect} from 'react'
 import Card from "./Card"
 
-  const CardCollection = ({onClick,pokemons,setPokemons} ) => {
+  const CardCollection = ({onClick, pokemons, setPokemons} ) => {
   
     const [loading, setLoading] = useState(true)
     
-    const getPokemonPool = (num) => {
-      console.log(`returns ${num} pokemons`)
-    }
+    // async function getPokemonPool(){
+    //   const arr = [
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //     fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+    //   ]
+
+    //   const res =  await Promise.all(arr)
+    //   const data = await  Promise.all(res.map((item) =>{return item.json()}))
+    //   console.log(data)
+    //   setPokemons(data)
+    //   setLoading(false)
+
+    // }
 
     useEffect(() =>{
-     
-      const arr = [
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
-      ]
-
-      Promise.all(arr)
-        .then((res) => {
-          Promise.all(res.map((item) =>{
-           return item.json()
+      async function getPokemonPool(){
+        const arr = [
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+          fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random()*700)}`),
+        ]
+  
+        // const res =  await Promise.all(arr)
+        // const data = await  Promise.all(res.map((item) => {
+        //   return item.json()}))
+        Promise.all(arr)
+        .then(res => {
+          Promise.all(res.map(item => {
+            return item.json()
           }))
           .then(data => console.log(data))
+          .then(data => setPokemons(data))
+          .then(setLoading(false))
         })
+      }
 
-    },[])
+      getPokemonPool()
 
-    //       console.log("fetching pokemons")
-    //       const randomNum =  await getRandomNum()
-    //       const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-    //       const data = await  result.json()
-    //       setPokemons(data)
-    //       setLoading(false)
-
+    },[setPokemons])
 
        return(
         <div>
+
           {loading ? 
           <div>Card collection is loading</div> 
           :   
-            <Card onClick={onClick} pokemon={pokemons}></Card>
+         <div>
+          {pokemons.map( poke => {
+            <Card  key={poke.name} onClick={onClick} pokemon={poke}></Card>
+          })}
+         </div>
           }
         </div>
        )
